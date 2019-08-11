db683000d8583b6149a62cea43e53356e3801c0c
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-12 01:47:33 +0530

    module.exports in Linked List

--- ds/LinkedList/LinkedList.js
+++ ds/LinkedList/LinkedList.js
@@ -83,22 +83,4 @@ LinkedList.prototype.indexOf = function (value) {
     return indexes;
 }
 
-var ll = new LinkedList();
-
-ll.addToHead(100);
-ll.addToHead(200);
-ll.addToHead(300);
-
-console.log(ll.removeHead());
-
-ll.addToTail(700);
-ll.addToTail(800);
-ll.addToTail(800);
-ll.addToTail(900);
-ll.addToTail(800);
-ll.addToTail(800);
-console.log(ll.removeTail());
-
-console.log('Search => ', ll.search(800))
-console.log('Index => ', ll.indexOf(800))
-// console.log(ll);
+module.exports = LinkedList;
---
+++ ds/LinkedList/Test.js
@@ -0,0 +1,21 @@
+const LinkedList = require('./LinkedList');
+
+var ll = new LinkedList();
+
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+
+console.log(ll.removeHead());
+
+ll.addToTail(700);
+ll.addToTail(800);
+ll.addToTail(800);
+ll.addToTail(900);
+ll.addToTail(800);
+ll.addToTail(800);
+console.log(ll.removeTail());
+
+console.log('Search => ', ll.search(800))
+console.log('Index => ', ll.indexOf(800))
+// console.log(ll);
