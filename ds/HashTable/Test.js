5ccdb5f73dbf56cb3625c8f2671055c2da0fa413
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-12 02:50:39 +0530

    insert func on HashTable

--- ds/HashTable/Test.js
+++ ds/HashTable/Test.js
@@ -2,4 +2,10 @@ const Map = require('./HashTable');
 
 var myHT = new Map(30);
 
+myHT.insert('Shubham', 23);
+myHT.insert('Divy', 12);
+myHT.insert('Virat', 30);
+
 console.log(myHT.hash('Becca'));
+
+console.log(myHT);
