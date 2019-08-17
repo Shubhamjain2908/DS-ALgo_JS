82e5bd810d2c85c0e9e22f1adb8e852eab8005e9
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-18 00:33:24 +0530

    Button5: using let

--- interview/Button5/Button5.js
+++ interview/Button5/Button5.js
@@ -1,18 +1,14 @@
 function createButtons() {
-  for (var i = 1; i <= 5; i++) {
+  // it is block scope
+  for (let i = 1; i <= 5; i++) {
     var body = document.getElementsByTagName("BODY")[0];
     var button = document.createElement("BUTTON");
     button.innerHTML = 'Button ' + i;
-    addClickFunctionality(button, i);
+    button.onclick = function () {
+      alert('This is button ' + i);
+    };
     body.appendChild(button);
   }
 }
-
-const addClickFunctionality = (button, num) => {
-  button.onclick = () => {
-    alert('This is button ' + num);
-  };
-}
-
 
 createButtons();
