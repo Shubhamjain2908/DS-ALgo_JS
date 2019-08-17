881e6a81d0abf26bde4e40b97888d1cd026ffda3
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-18 02:49:19 +0530

    TripleAdd: aaa & aaaa

--- interview/1.TrippleAddFunction.js
+++ interview/1.TrippleAddFunction.js
@@ -19,11 +19,14 @@ const tripleAddES6 = (num1) => {
     }
 }
 
+const aaa = num1 => num2 => num3 => num1 + num2 + num3;
+
 // returns total of all 3 numbers added together
 const t = tripleAdd(10)(20)(30);
 const t2 = tripleAddES6(10)(20)(30);
 
 console.log(t, t2);
+console.log(aaa(29)(82)(197));
 
 // Quadruple Function
 const quadrupleAdd = (num1) => {
@@ -34,4 +37,7 @@ const quadrupleAdd = (num1) => {
     }
 };
 
-console.log(quadrupleAdd(22)(34)(83, 11))
+const aaaa = num1 => num2 => (num3, num4) => num1 + num2 + num3 + num4;
+
+console.log(quadrupleAdd(22)(34)(83, 11));
+console.log(aaaa(32)(72)(40, 59));
