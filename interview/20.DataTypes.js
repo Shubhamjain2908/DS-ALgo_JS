8a925429448be6526d3e4bde4ef67220b8d21678
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-22 21:00:45 +0530

    data types

---
+++ interview/20.DataTypes.js
@@ -0,0 +1,8 @@
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object - In JS arrays are objects
+
+// Methods to check if an object is array or not
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
