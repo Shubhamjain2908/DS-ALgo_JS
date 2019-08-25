1cb2bfe4e37a9b988a5697ae8f058f35b9e63e4b
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-25 21:20:55 +0530

    add

---
+++ extras/AlternateAddSubtract.js
@@ -0,0 +1,11 @@
+let counter = 0;
+function addSubtract(num) {
+    counter++;
+}
+addSubtract(2);
+addSubtract(2);
+addSubtract(2);
+addSubtract(2);
+addSubtract(2);
+addSubtract(2);
+console.log(counter);
