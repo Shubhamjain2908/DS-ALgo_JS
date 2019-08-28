ec43fb1a63766b757eb1e76e5e49e5ec59f1750a
Shubham Jain <shubham.jain@engineerbabu.in>
2019-08-28 21:25:18 +0530

    stack implementation

---
+++ ds/Stack/Stack.js
@@ -0,0 +1,27 @@
+function Stack() {
+    this.arr = [];
+}
+
+Stack.prototype.push = function (value) {
+    this.arr.push(value);
+}
+
+Stack.prototype.pop = function () {
+    if (this.arr.length === 0)
+        throw new Error('Stack Underflow');
+    this.arr.pop();
+}
+
+Stack.prototype.isEmpty = function () {
+    return this.arr.length === 0;
+}
+
+Stack.prototype.peek = function () {
+    return this.arr[this.arr.length - 1];
+}
+
+Stack.prototype.print = function () {
+    console.log(this.arr);
+}
+
+module.exports = Stack;
---
+++ ds/Stack/Test.js
@@ -0,0 +1,9 @@
+const Stack = require('./Stack');
+
+let st = new Stack();
+st.push(21);
+st.push(23);
+st.push(22);
+st.push(29);
+console.log(st.peek());
+st.print();
