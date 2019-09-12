acf6e4f1fcedbbc3d14e3d91d10abe69d53c55a6
Shubham Jain <shubham.jain@engineerbabu.in>
2019-09-12 22:05:57 +0530

    edit changes DP

--- DailyInterviewPro/P016_EditDistanceStr.js
+++ DailyInterviewPro/P016_EditDistanceStr.js
@@ -6,53 +6,26 @@
     For example, "biting" and "sitting" have an edit distance of 2 (substitute b for s, and insert a t).
  */
 
-const lcs = (str1, str2) => {
+const editDistance = (str1, str2) => {
     str1 = [...str1];
     str2 = [...str2];
     const m = str1.length;
     const n = str2.length;
-    let L = new Array(m + 1).fill(new Array(n + 1));
-    for (let i = 0; i <= m; i++) {
-        for (let j = 0; j <= n; j++) {
-            if (i == 0 || j == 0) {
-                L[i][j] = 0;
-            } else if (str1[i - 1] == str2[j - 1]) {
-                L[i][j] = L[i - 1][j - 1] + 1;
-            } else {
-                L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
-            }
-        }
-    }
-    return L[m][n];
+    return editDistance_dp(str1, str2, m, n);
 }
 
-const editDistance = (str1, str2) => {
-    // str1 = [...str1];
-    // str2 = [...str2];
-    const m = str1.length;
-    const n = str2.length;
-    // let dp = new Array(n + 1).fill(new Array(m + 1));
-
-    // for (let i = 0; i < m + 1; i++) {
-    //     dp[0][i] = i;
-    // }
-
-    // for (let i = 0; i < n + 1; i++) {
-    //     dp[i][0] = i;
-    // }
-
-    // for (let i = 1; i <= str1.length; i++) {
-    //     for (let j = 1; j <= str2.length; j++) {
-    //         if (str1[i - 1] == str2[j - 1]) {
-    //             dp[i][j] = dp[i - 1][j - 1];
-    //         } else {
-    //             dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
-    //         }
-    //     }
-    // }
-    // console.log(dp);
-    const l = lcs(str1, str2);
-    return (m - l) + (n - l);
+const editDistance_dp = (str1, str2, m, n) => {
+    if (m === 0) {
+        return n;
+    }
+    if (n === 0) {
+        return m;
+    }
+    if (str1[m - 1] === str2[n - 1]) {
+        return editDistance_dp(str1, str2, m - 1, n - 1);
+    } else {
+        return 1 + Math.min(editDistance_dp(str1, str2, m - 1, n - 1), Math.min(editDistance_dp(str1, str2, m, n - 1), editDistance_dp(str1, str2, m - 1, n)));
+    }
 }
 
 const a = 'biting';
