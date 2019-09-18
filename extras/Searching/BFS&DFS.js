cb4384f923bac9b7e9a7167dd5691b1d99e96fd2
Shubham Jain <shubham.jain@engineerbabu.in>
2019-09-19 02:13:15 +0530

    bfs dfs question

---
+++ extras/Searching/BFS&DFS.js
@@ -0,0 +1,30 @@
+/**
+ * Q. If you know the solution is not far from the root of thr tree.
+ * A. BFS
+ */
+
+
+/**
+ * Q. If the tree is very deep & solutions are rare.
+ * A. BFS (DFS will take long)
+ */
+
+/**
+ * Q. If the tree is very wide.
+ * A. DFS (BFS will need too much memory)
+ */
+
+/**
+ * Q. If solutions are frequent but located deep in the tree.
+ * A. DFS
+ */
+
+/**
+* Q. Determining whether the path exists between two nodes.
+* A. DFS
+*/
+
+/**
+* Q. Finding shortest path.
+* A. BFS
+*/
