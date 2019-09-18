8945ccef5c4c7c36a25d5f42d2ca608f02752284
Shubham Jain <shubham.jain@engineerbabu.in>
2019-09-19 01:45:49 +0530

    xhr requests

--- extras/xhr-fetch-axios/xhr.js
+++ extras/xhr-fetch-axios/xhr.js
@@ -1,9 +1,50 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => { };
+const sendHttpRequest = (method, url, data = {}) => {
 
-const sendData = () => { };
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+
+        xhr.responseType = 'json';
+        if (data) {
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        }
+
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response);
+            }
+            resolve(xhr.response);
+        };
+
+        xhr.onerror = () => {
+            reject('Something went wrong!!!');
+        };
+
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+}
+
+const getData = () => {
+    sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
+        console.log(responseData);
+    });
+};
+
+const sendData = () => {
+    const obj = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    };
+    sendHttpRequest('POST', 'https://reqres.in/api/register', obj).then(responseData => {
+        console.log(responseData);
+    })
+        .catch(err => console.log(err));
+
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
