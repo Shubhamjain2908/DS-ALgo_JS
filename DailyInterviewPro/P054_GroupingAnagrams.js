33f0216eb2b4c3c0c0f65fad6080fb1800a0fe33
Shubham Jain <shubham.jain@engineerbabu.in>
2019-10-19 17:00:32 +0530

    Group Words that are Anagrams

---
+++ DailyInterviewPro/P054_GroupingAnagrams.js
@@ -0,0 +1,56 @@
+/**
+ * AirBNB
+ * 
+ * Given a list of words, group the words that are anagrams of each other. (An anagram are words made up of the same letters).
+Example:
+    Input: ['abc', 'bcd', 'cba', 'cbd', 'efg']
+    Output: [['abc', 'cba'], ['bcd', 'cbd'], ['efg']]
+ */
+
+const groupAnagrams = words => {
+    const len = words.length;
+    const checkAnagram = (A, B) => {
+        let charMap = new Map();
+        for (const key of A) {
+            if (charMap.has(key)) {
+                charMap.set(key, charMap.get(key) + 1);
+            } else {
+                charMap.set(key, 1);
+            }
+        }
+        for (const key of B) {
+            if (charMap.has(key)) {
+                let value = charMap.get(key);
+                charMap.set(key, value - 1);
+                if (value === 1) {
+                    charMap.delete(key);
+                }
+            } else {
+                return false;
+            }
+        }
+        return !charMap.size;
+    }
+    let visited = new Array(len).fill(false);
+    let result = [];
+    for (let i = 0; i < len; i++) {
+        const e = words[i];
+        let temp = [];
+        if (!visited[i]) {
+            visited[i] = true;
+            temp.push(e);
+        }
+        for (let j = 0; j < len; j++) {
+            const f = words[j];
+            if (!visited[j] && checkAnagram(e, f)) {
+                visited[j] = true;
+                temp.push(f);
+            }
+        }
+        temp.length ? result.push(temp) : '';
+    }
+    return result;
+}
+
+const words = ['abc', 'bcd', 'cba', 'cbd', 'efg'];
+console.log(groupAnagrams(words));
