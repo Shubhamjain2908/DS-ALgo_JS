80d8f3031b2230c9c7fd48a0cb873094ce423072
Shubham Jain <shubham.jain@engineerbabu.in>
2019-10-19 17:03:31 +0530

    comments in P054

--- DailyInterviewPro/P054_GroupingAnagrams.js
+++ DailyInterviewPro/P054_GroupingAnagrams.js
@@ -7,30 +7,42 @@ Example:
     Output: [['abc', 'cba'], ['bcd', 'cbd'], ['efg']]
  */
 
+/**
+ * Utility function to check whether 2 strings are anagrams of each other
+ * @param {String} A 
+ * @param {String} B 
+ * @returns {boolean} isAnagram
+ */
+const checkAnagram = (A, B) => {
+    let charMap = new Map();
+    for (const key of A) {
+        if (charMap.has(key)) {
+            charMap.set(key, charMap.get(key) + 1);
+        } else {
+            charMap.set(key, 1);
+        }
+    }
+    for (const key of B) {
+        if (charMap.has(key)) {
+            let value = charMap.get(key);
+            charMap.set(key, value - 1);
+            if (value === 1) {
+                charMap.delete(key);
+            }
+        } else {
+            return false;
+        }
+    }
+    return !charMap.size;
+}
+
+/**
+ * Program to group the words that are anagrams of each other
+ * @param {Array} words
+ * @returns {Array} groupedArrays
+ */
 const groupAnagrams = words => {
     const len = words.length;
-    const checkAnagram = (A, B) => {
-        let charMap = new Map();
-        for (const key of A) {
-            if (charMap.has(key)) {
-                charMap.set(key, charMap.get(key) + 1);
-            } else {
-                charMap.set(key, 1);
-            }
-        }
-        for (const key of B) {
-            if (charMap.has(key)) {
-                let value = charMap.get(key);
-                charMap.set(key, value - 1);
-                if (value === 1) {
-                    charMap.delete(key);
-                }
-            } else {
-                return false;
-            }
-        }
-        return !charMap.size;
-    }
     let visited = new Array(len).fill(false);
     let result = [];
     for (let i = 0; i < len; i++) {
